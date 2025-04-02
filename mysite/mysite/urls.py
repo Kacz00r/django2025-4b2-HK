a2c8b5a547eaed78a9b85a7d4f09f87500840252
Hubert Kaczmarek <hubertkaczor44@gmail.com>
2025-04-02 07:59:00 +0200

    dodanie landing page - chyba

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 urlpatterns = [
+    path('', views.landing_page, name="landing_page"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
